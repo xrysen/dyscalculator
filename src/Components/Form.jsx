6e862be27c004d7646aff171a5e4d93502c5051d
Sean Oyler <xrysen@gmail.com>
2021-04-27 16:05:45 -0700

    Performs calculations based on selected options

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,22 +1,112 @@
 import "./Form.css";
-import Bill from "./Bill";
+import { useEffect, useState } from "react";
+import Option from "./Option";
 
 const Form = () => {
+  const [total, setTotal] = useState(0);
+  const [hundred, setHundred] = useState(0);
+  const [fifty, setFifty] = useState(0);
+  const [twenty, setTwenty] = useState(0);
+  const [ten, setTen] = useState(0);
+  const [five, setFive] = useState(0);
+  const [one, setOne] = useState(0);
+
+  const handleBillChange = (bill, e) => {
+    switch (bill) {
+      case 1:
+        setOne(1 * e.target.value);
+        break;
+      case 5:
+        setFive(5 * e.target.value);
+        break;
+
+      case 10:
+        setTen(10 * e.target.value);
+        break;
+
+      case 20:
+        setTwenty(20 * e.target.value);
+        break;
+
+      case 50:
+        setFifty(50 * e.target.value);
+        break;
+
+      case 100:
+        setHundred(100 * e.target.value);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  useEffect(() => {
+    setTotal(one + five + ten + twenty + fifty + hundred)
+  }, [one, five, ten, twenty, fifty, hundred]);
+
   return (
     <div className="form">
       <h1>What's in Your Wallet?</h1>
       <table>
-        <tr>
-          <th>Bill</th>
-          <th>Amount</th>
-        </tr>
-        <Bill bill = "$100 Bills" />
-        <Bill bill = "$50 Bills" />
-        <Bill bill = "$20 Bills" />
-        <Bill bill = "$10 Bills" />
-        <Bill bill = "$5 Bills" />
-        <Bill bill = "$1 Bills" />
+        <thead>
+          <tr>
+            <th>Bill</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>$100 Bills</td>
+            <td>
+              <select onChange={(e) => handleBillChange(100, e)}>
+                <Option />
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>$50 Bills</td>
+            <td>
+              <select onChange={(e) => handleBillChange(50, e)}>
+                <Option />
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>$20 Bills</td>
+            <td>
+              <select onChange={(e)=> handleBillChange(20, e)}>
+                <Option />
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>$10 Bills</td>
+            <td>
+              <select onChange={(e)=> handleBillChange(10, e)}>
+                <Option />
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>$5 Bills</td>
+            <td>
+              <select onChange={(e)=> handleBillChange(5, e)}>
+                <Option />
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>$1 Bills</td>
+            <td>
+              <select onChange={(e)=> handleBillChange(1, e)}>
+                <Option />
+              </select>
+            </td>
+          </tr>
+        </tbody>
       </table>
+      <p>Total: ${total}</p>
     </div>
   );
 };
--- src/Components/Option.jsx
+++ src/Components/Option.jsx
@@ -1,13 +1,12 @@
 const Option = (props) => {
   const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
-    <div>
-      <select>
+    <>
         {nums.map((item) => (
-          <option value={item}>{item}</option>
+          <option key={item} value={item}>{item}</option>
         ))}
-      </select>
-    </div>
+      </>
   );
 };
 
