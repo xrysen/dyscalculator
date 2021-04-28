271bfa99638e4cd7cf9ff9726f957798fa4c1f78
Sean Oyler <xrysen@gmail.com>
2021-04-28 01:06:19 -0700

    Adds change calculations

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -22,33 +22,49 @@ const Form = () => {
       case 1:
         setBills({
           ...bills,
-          one: 1 * e.target.value,
+          one: 1 * Number(e.target.value),
         });
         break;
       case 5:
         setBills({
           ...bills,
-          five: 5 * e.target.value,
+          five: 5 * Number(e.target.value),
         });
         break;
 
       case 10:
         setBills({
           ...bills,
-          ten: 10 * e.target.value,
+          ten: 10 * Number(e.target.value),
         });
         break;
 
       case 20:
-        setBills({ ...bills, twenty: 20 * e.target.value });
+        setBills({ ...bills, twenty: 20 * Number(e.target.value) });
         break;
 
       case 50:
-        setBills({ ...bills, fifty: 50 * e.target.value });
+        setBills({ ...bills, fifty: 50 * Number(e.target.value) });
         break;
 
       case 100:
-        setBills({ ...bills, hundred: 100 * e.target.value });
+        setBills({ ...bills, hundred: 100 * Number(e.target.value) });
+        break;
+
+      case "quarter":
+        setBills({ ...bills, quarter: 0.25 * Number(e.target.value) });
+        break;
+
+      case "dime":
+        setBills({ ...bills, dime: 0.1 * Number(e.target.value) });
+        break;
+
+      case "nickel":
+        setBills({ ...bills, nickel: 0.05 * Number(e.target.value) });
+        break;
+
+      case "penny":
+        setBills({ ...bills, penny: 0.01 * Number(e.target.value) });
         break;
 
       default:
@@ -57,11 +73,11 @@ const Form = () => {
   };
 
   useEffect(() => {
-     let sum = 0;
-     for (const key in bills) {
-       sum += bills[key];
-     }
-     setTotal(sum);
+    let sum = 0;
+    for (const key in bills) {
+      sum += bills[key];
+    }
+    setTotal(sum);
   }, [bills]);
 
   return (
@@ -84,11 +100,23 @@ const Form = () => {
                 <Option />
               </select>
             </td>
+            <td>Quarters</td>
+            <td>
+              <select onChange={(e) => handleBillChange("quarter", e)}>
+                <Option />
+              </select>
+            </td>
           </tr>
           <tr>
             <td>$50 Bills</td>
             <td>
               <select onChange={(e) => handleBillChange(50, e)}>
+                <Option />
+              </select>
+            </td>
+            <td>Dimes</td>
+            <td>
+              <select onChange={(e) => handleBillChange("dime", e)}>
                 <Option />
               </select>
             </td>
@@ -100,11 +128,23 @@ const Form = () => {
                 <Option />
               </select>
             </td>
+            <td>Nickels</td>
+            <td>
+              <select onChange={(e) => handleBillChange("nickel", e)}>
+                <Option />
+              </select>
+            </td>
           </tr>
           <tr>
             <td>$10 Bills</td>
             <td>
               <select onChange={(e) => handleBillChange(10, e)}>
+                <Option />
+              </select>
+            </td>
+            <td>Pennies</td>
+            <td>
+              <select onChange={(e) => handleBillChange("penny", e)}>
                 <Option />
               </select>
             </td>
@@ -127,7 +167,7 @@ const Form = () => {
           </tr>
         </tbody>
       </table>
-      <p>Total: ${total}</p>
+      <p>Total: ${total.toFixed(2)}</p>
     </div>
   );
 };
